3d047eee58e87bdfeef507617e0449808075c75f
이정훈 <eu4ng97@gmail.com>
2025-03-14 11:36:48 +0900

    Feat: MonoSingleton 클래스에 초기화 기능 추가

--- package/Runtime/MonoSingleton.cs
+++ package/Runtime/MonoSingleton.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 namespace Eu4ng.Manager.Singleton
 {
+    /// <summary>
+    /// MonoBehaviour 기반의 제네릭 싱글톤 클래스입니다.
+    /// Instance 프로퍼티 호출 시 초기화 완료 상태를 보장합니다.
+    /// </summary>
     public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
         private static T s_Instance;
 
-        public static T Instance => s_Instance ?? FindInstance() ?? CreateInstance();
+        public static T Instance
+        {
+            get
+            {
+                var instance = s_Instance ?? FindInstance() ?? CreateInstance();
+                instance.Initialize();
+
+                return instance;
+            }
+        }
+
+        public bool IsInitialized { get; private set; }
+
+        /* MonoSingleton */
 
         private static T FindInstance()
         {
@@ -22,6 +39,19 @@ namespace Eu4ng.Manager.Singleton
 
             return s_Instance;
         }
+
+        private void Initialize()
+        {
+            if (IsInitialized) return;
+
+            Debug.Log(typeof(T).Name + " is initialized.");
+            IsInitialized = true;
+            OnInitialize();
+        }
+
+        protected virtual void OnInitialize() {}
+
+        /* MonoBehaviour */
 
         protected virtual void Awake()
         {
